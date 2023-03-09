ffdcb86fb3413301ced45a0143225efc0467107a
kavindushehan <it20076566@my.sliit.lk>
2023-03-09 21:19:19 +0530

    Added images

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -3,26 +3,19 @@ import 'package:ctse_app/screens/login.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-
 class Wrapper extends StatelessWidget {
   const Wrapper({Key? key}) : super(key: key);
 
-
-
   @override
   Widget build(BuildContext context) {
-
     final FirebaseAuth auth = FirebaseAuth.instance;
 
     final user = auth.currentUser;
-    print(user);
-    if(user!=null){
 
+    if (user != null) {
       return const Home();
+    } else {
+      return const EmailSignin();
     }
-    else{
-      return  const EmailSignin();
-    }
-
   }
 }
