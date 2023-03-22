8ccbe478f810e90f1e0a2b502c052c0f32cd4e23
kavindushehan <it20076566@my.sliit.lk>
2023-03-22 22:13:41 +0530

    Remove warnings

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -15,16 +15,16 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
     Timer(
-        Duration(seconds: 2),
+        const Duration(seconds: 2),
         () => Navigator.pushReplacement(
-            context, MaterialPageRoute(builder: (context) => Wrapper())));
+            context, MaterialPageRoute(builder: (context) => const Wrapper())));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        decoration: BoxDecoration(color: Colors.black),
+        decoration: const BoxDecoration(color: Colors.black),
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -35,8 +35,8 @@ class _SplashScreenState extends State<SplashScreen> {
                 width: 200,
                 height: 200,
               ),
-              SizedBox(height: 20),
-              CircularProgressIndicator(
+              const SizedBox(height: 20),
+              const CircularProgressIndicator(
                 valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
               ),
             ],
