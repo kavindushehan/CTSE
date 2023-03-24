25b87707ef8eed8f224f7a8d96523e9dfdc8f333
Ariyarathna H.M.M.M <it20120634@my.sliit.lk>
2023-03-24 14:18:11 +0530

    model update note

--- lib/models/note.dart
+++ lib/models/note.dart
@@ -1,25 +1,32 @@
 class Notes {
   final String noteId;
   String noteDescription;
+  String noteTitle;
+  String noteType;
   
 
   Notes({
     required this.noteId,
+    required this.noteTitle,
     required this.noteDescription,
+    required this.noteType,
     
   });
 
   factory Notes.fromJson(Map<String, dynamic> json) {
     return Notes(
       noteId: json['noteId'] as String,
+      noteTitle: json['noteTitle'] as String,
       noteDescription: json['noteDescription'] as String,
-      
+      noteType: json['noteType'] as String,
     );
   }
 
   Map<String, dynamic> toJson() => {
         'noteId': noteId,
+        'noteTitle': noteTitle,
         'noteDescription': noteDescription,
+        'noteType': noteType,
         
       };
 }
